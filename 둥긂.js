848bc18fd72a04b69e910fffe0972ef492829678
vlzi <76029766+vlzi@users.noreply.github.com>
2021-03-11 22:29:27 +0900

    Update 둥긂.js

--- 둥긂.js
+++ 둥긂.js
@@ -2,6 +2,12 @@
 	console.log("둥긂 불러오는 중...");
 	const util = await import("https://vlzi.github.io/entry/둥긂_utils.js");//await import("./둥긂_utils.js");
 	const vc = Entry.variableContainer;
+	const fvname = () => vc.variables_.length ? vc.variables_[0].name : "대상 없음";
+	function getv(name) {
+		const variable = vc.getVariableByName(name);
+		if (variable) return variable;
+		else util.raise("변수를 찾을 수 없습니다.");
+	}
 	util.change("set_visible_answer", [{num : 0, default : "숨기기"}], (sprite, script) => {
 		const bool = script.getValue('BOOL', script);
 		if (bool === '숨기기') {
@@ -12,10 +18,67 @@
 		else util.raise("숨기기/보이기를 입력하세요.");
 		return script.callReturn();
 	});
-	util.change("get_variable", [{num : 0, default : vc.variables_.length ? vc.variables_[0].name_ : "대상 없음"}], (sprite, script) => {
-		const variable = vc.getVariableByName(script.getValue("VARIABLE"));
-		if (variable) return variable.getValue();
-		else util.raise("변수를 찾을 수 없습니다.");
+	util.change("get_variable", [{num : 0, default : fvname()}], (sprite, script) => getv(script.getValue("VARIABLE")).getValue());
+	util.change("change_variable", [{num : 0, default : fvname()}], function func(sprite, script) { //복붙하고 고쳤습니다.
+		var value = script.getValue('VALUE', script);
+		var fixed = 0;
+
+		if (value == false && typeof value === 'boolean') {
+		throw new Error('Type is not correct');
+		}
+
+		var variable = getv(script.getValue('VARIABLE', script));
+		var isRealTime_ = variable.isRealTime_;
+		var variableValue = variable.getValue();
+		var sumValue;
+
+		if (Entry.Utils.isNumber(value) && variable.isNumber()) {
+		value = Entry.parseNumber(value);
+		variableValue = Entry.parseNumber(variableValue);
+		fixed = Entry.getMaxFloatPoint([value, variable.getValue()]);
+		sumValue = new BigNumber(value).plus(variableValue).toNumber().toFixed(fixed);
+		} else {
+		sumValue = "".concat(variableValue).concat(value);
+		}
+
+		if (!isRealTime_) {
+		variable.setValue(sumValue);
+		return script.callReturn();
+		} else {
+		return new Promise( /*#__PURE__*/function () {
+		var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(resolve, reject) {
+		return _regenerator["default"].wrap(function _callee$(_context) {
+		  while (1) {
+		    switch (_context.prev = _context.next) {
+		      case 0:
+			_context.prev = 0;
+			_context.next = 3;
+			return variable.setValue(sumValue);
+
+		      case 3:
+			resolve();
+			_context.next = 9;
+			break;
+
+		      case 6:
+			_context.prev = 6;
+			_context.t0 = _context["catch"](0);
+			reject(_context.t0);
+
+		      case 9:
+		      case "end":
+			return _context.stop();
+		    }
+		  }
+		}, _callee, null, [[0, 6]]);
+		}));
+
+		return function (_x, _x2) {
+		return _ref.apply(this, arguments);
+		};
+		}());
+		}
+		}
 	});
 	util.update();
 	console.log("둥긂 불러오기 완료");
