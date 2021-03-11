2a46c580e19039df4bab96a45822dddea40d3ff0
vlzi <76029766+vlzi@users.noreply.github.com>
2021-03-11 22:01:11 +0900

    Update 둥긂.js

--- 둥긂.js
+++ 둥긂.js
@@ -1,6 +1,7 @@
 (async () => {
 	console.log("둥긂 불러오는 중...");
 	const util = await import("./둥긂_utils.js");
+	const vc = Entry.variableContainer;
 	util.change("set_visible_answer", [{num : 0, default : "숨기기"}], (sprite, script) =>
 	{
 		const bool = script.getValue('BOOL', script);
@@ -15,6 +16,8 @@
 		}
 		return script.callReturn();
 	});
+	util.change("get_variable", [{num : 0, default : vc.variables_.length ? vc.variables[0].name_ : "대상 없음"}], (sprite, script) =>
+		    vc.getVariableByName(script.getValue("VARIABLE")).getValue());
 	util.update();
 	console.log("둥긂 불러오기 완료");
 	console.log(Entry.playground.blockMenu._categoryData);
