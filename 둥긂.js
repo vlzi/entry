207d95f50adce1bf6ca0d49ca938a32e912da27c
vlzi <76029766+vlzi@users.noreply.github.com>
2021-03-11 21:07:29 +0900

    Update 둥긂.js

--- 둥긂.js
+++ 둥긂.js
@@ -1,18 +1,19 @@
-(async () => {
-	const util = await import("./둥긂_utils.js");
-	util.change("set_visible_answer", [{num : 0, default : "숨기기"}], (sprite, script) =>
-	{
-		const bool = script.getValue('BOOL', script);
-		if (bool === '숨기기') {
-		Entry.container.inputValue.setVisible(false);
-		} else if (bool === '보이기'){
-			Entry.container.inputValue.setVisible(true);
-		}
-		else {
-			Entry.toast.alert("필드 값 오류", "숨기기/보이기를 입력하세요.", true);
-			throw new Error();
-		}
-		return script.callReturn();
-	});
-	util.update();
-})();
+console.log("둥긂 불러오는 중...");
+const util = await import("./둥긂_utils.js");
+util.change("set_visible_answer", [{num : 0, default : "숨기기"}], (sprite, script) =>
+{
+	const bool = script.getValue('BOOL', script);
+	if (bool === '숨기기') {
+	Entry.container.inputValue.setVisible(false);
+	} else if (bool === '보이기'){
+		Entry.container.inputValue.setVisible(true);
+	}
+	else {
+		Entry.toast.alert("필드 값 오류", "숨기기/보이기를 입력하세요.", true);
+		throw new Error();
+	}
+	return script.callReturn();
+});
+util.update();
+console.log("둥긂 불러오기 완료");
+console.log(Entry.playground.blockMenu._categoryData);
